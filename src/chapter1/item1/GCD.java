e1f132f336c1694e41143bfbfc5378e8633ae60f
Kelly Lin <xiaoyumumu911@163.com>
2019-03-09 12:33:32 +0800

    Greatest common divisor

--- src/chapter1/item1/GCD.java
+++ src/chapter1/item1/GCD.java
@@ -1,4 +1,13 @@
 package chapter1.item1;
 
 public class GCD {
+    public static int gcd(int p, int q) {
+        if (q == 0) return p;
+        int r = p % q;
+        return gcd(q, r);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcd(126,36));
+    }
 }
