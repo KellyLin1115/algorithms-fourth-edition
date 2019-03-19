06a4f68d6eab596c6b0d41d95a9ac7f09c2d63ef
Kelly Lin <xiaoyumumu911@163.com>
2019-03-19 12:38:44 +0800

    Add selection algorithm

--- src/chapter2/item1/Selection.java
+++ src/chapter2/item1/Selection.java
@@ -1,4 +1,42 @@
 package chapter2.item1;
 
+import java.util.Random;
+
 public class Selection {
+
+    private Selection() {}
+
+    public static void sort(Comparable[] a){
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int min = i;
+            for (int j = i+1; j < n; j++) {
+                if(a[j].compareTo(a[min]) < 0)
+                    min = j;
+            }
+            exch(a, i, min);
+        }
+
+    }
+
+    private static void exch(Comparable[] a, int i, int j){
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    public static void main(String[] args) {
+        Integer[] a = new Integer[10];
+        Random rand = new Random();
+        for (int i = 0; i < a.length; i++) {
+            a[i] = rand.nextInt(100);
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+        System.out.println("After sort: ");
+        sort(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
 }
